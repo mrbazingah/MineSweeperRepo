dd1c26b6266188e40986bf2f8459a33e43a601ca
mrbazingah <mrbazingah@gmail.com>
2025-07-04 13:46:06 +0200

    Added build but cant see it in github

--- MineSweeper/Assets/Scripts/GreenGridManager.cs
+++ MineSweeper/Assets/Scripts/GreenGridManager.cs
@@ -1,5 +1,4 @@
-﻿// GreenGridManager.cs
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
---
+++ MineSweeper/Assets/Scripts/TODO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TODO : MonoBehaviour
+{
+    //Win Screen
+    //Start menu
+    //Settings menu
+    //Add a timer
+    //Add Audio
+    //Add a score system
+    //Add difficulty levels
+    //Quit Button
+    //Pause Menu
+}
